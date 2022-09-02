1faadbaf950c48173763fd3ef377852dc951ef4e
Svetlana Levkovskaya <levkovskayase@gmail.com>
2022-09-02 21:16:38 +0300

    api-tests

--- test/posts.js
+++ test/posts.js
@@ -14,7 +14,7 @@ describe("User Posts", () => {
     userId = await createRandomUser();
   });
 
-  it("/posts", async() => {
+  it("POST /posts", async () => {
     const data = {
       user_id: userId,
       title: "Svetlana Title",
@@ -31,14 +31,50 @@ describe("User Posts", () => {
     postId = postRes.body.id;
     //console.log("postId", postId);
   });
-});
 
-it("GET /posts/:id", async () => {
-  const myposts = await request
-    .get("posts/1333")
-    //.get(`posts/${postId}`)
-    .set("Authorization", `Bearer${TOKEN}`)
-    .expect(200);
-  //console.log("userId", myposts.body.user_id);
-  //console.log("postId ", myposts.body.id);
+  it("GET /posts/:id", async () => {
+    const myposts = await request
+      //.get("posts/1333")
+      .get(`posts/${postId}`)
+      .set("Authorization", `Bearer${TOKEN}`)
+      .expect(404);
+    //console.log("userId", myposts.body.user_id);
+    //console.log("postId ", myposts.body.id);
+  });
+
+  describe("Negative tests", () => {
+    it("401 Authentication", async () => {
+      const data = {
+        user_id: userId,
+        title: "Svetlana Title",
+        body: "Svetlana blog post",
+      };
+
+      const postRes = await request.post("posts").send(data);
+
+      console.log(postRes.statusCode);
+      console.log(postRes.body.message);
+      expect(postRes.statusCode).to.eq(401);
+      expect(postRes.body.message).to.eq("Authentication failed");
+    });
+
+    it.only("422 Validation failed", async () => {
+      const data = {
+        user_id: userId,
+        title: "Svetlana Title",
+        //body: "Svetlana blog post",
+      };
+
+      const postRes = await request
+        .post("posts")
+        .set("Authorization", `Bearer ${TOKEN}`)
+        .send(data);
+
+      console.log(postRes.statusCode);
+      console.log(postRes.body[0].message);
+      expect(postRes.statusCode).to.eq(422);
+      expect(postRes.body[0].field).to.eq("body");
+      expect(postRes.body[0].message).to.eq("can't be blank");
+    });
+  });
 });
